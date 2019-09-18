c5d6dfc5c6e811ba8b03702339417dedf630f0dd
Elijah Verdoorn <elijah.verdoorn@wework.com>
2019-09-18 12:16:53 -0700

    Add sample of Observable interoperability

---
+++ src/main/kotlin/interop/observable/ObservableInterop.kt
@@ -0,0 +1,18 @@
+package interop.observable
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.rx2.rxObservable
+import kotlin.coroutines.CoroutineContext
+
+class ObservableInterop(
+    override val coroutineContext: CoroutineContext = Dispatchers.Default
+): CoroutineScope {
+    fun getStream() = CoroutineScope(coroutineContext).rxObservable {
+        (1..10).map {
+            delay(100)
+            send(it)
+        }
+    }
+}
